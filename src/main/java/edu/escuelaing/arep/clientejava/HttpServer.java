1c8e069ff79da7e4a924a5edccb0cb78ff477d14
DamGaFlo <49254975+DamGaFlo@users.noreply.github.com>
2021-09-08 16:41:12 -0500

    caombio de puerto heroku

--- src/main/java/edu/escuelaing/arep/clientejava/HttpServer.java
+++ src/main/java/edu/escuelaing/arep/clientejava/HttpServer.java
@@ -28,7 +28,7 @@ public class HttpServer {
     public void start(String[] args) throws IOException, URISyntaxException {
         ServerSocket serverSocket = null;
         try {
-            serverSocket = new ServerSocket(35000);
+            serverSocket = new ServerSocket(getPort());
 
         } catch (IOException e) {
             System.err.println("Could not listen on port: 35000.");
@@ -48,6 +48,12 @@ public class HttpServer {
             clientSocket.close();
         }
         serverSocket.close();
+    }
+    static int getPort(){
+        if(System.getenv("PORT")!=null){
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
     }
 
     public void serverConnection(Socket clientSocket) throws IOException, URISyntaxException {
